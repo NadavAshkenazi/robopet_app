efd557880e5a34d9518a707b8c5d2a6246f602c2
abialystok <asaf.b@campus.technion.ac.il>
2021-05-08 15:13:38 +0300

    remove MyApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,13 @@ import 'package:control_pad/control_pad.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    );
-  }
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: HomePage(),
+  ));
 }
 
 class HomePage extends StatefulWidget {
